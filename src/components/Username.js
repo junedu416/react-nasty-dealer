1a947c193a2e5228830cdb9490479c5049103215
Nam-fullstack <namnguyen13_@hotmail.com>
2021-10-20 22:22:47 +1100

    Adds username form component.

---
+++ src/components/Username.js
@@ -0,0 +1,7 @@
+import TextField from '@mui/material/TextField';
+
+function Username() {
+  return <TextField id="outlined-basic" label="Enter name" variant="outlined" />;
+}
+
+export default Username;
