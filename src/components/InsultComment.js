2fe5ae3021d19b847ab2cf6cb4293dc1ccb2504a
Nam-fullstack <namnguyen13_@hotmail.com>
2021-10-21 16:08:24 +1100

    Changes display for cards to be inline/row, removes card value text.

--- src/components/InsultComment.js
+++ src/components/InsultComment.js
@@ -1,23 +1,23 @@
-import React, {useEffect, useState} from 'react';
-
+import React, { useEffect, useState } from "react";
 
 const InsultComment = () => {
-   const [insult, setInsult] = useState("")
+  const [insult, setInsult] = useState("");
 
-    useEffect(()=> {
-        fetch('https://clare-cors-server.herokuapp.com/https://evilinsult.com/generate_insult.php?lang=en&type=json', {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        .then(response => response.json())
-        .then(data => setInsult(data.insult))
-        .catch(error => console.error(error))
-    }, [])
-
-    return(
-        <p>my insult: {insult}</p>
+  useEffect(() => {
+    fetch(
+      "https://clare-cors-server.herokuapp.com/https://evilinsult.com/generate_insult.php?lang=en&type=json",
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
     )
-}
+      .then((response) => response.json())
+      .then((data) => setInsult(data.insult))
+      .catch((error) => console.error(error));
+  }, []);
 
-export default InsultComment
+  return <p>my insult: {insult}</p>;
+};
+
+export default InsultComment;
