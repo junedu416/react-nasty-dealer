23651dc211a10b3234071a289c31c5dc285fe514
daniel-walters <waltersdaniel96@gmail.com>
2021-10-21 10:01:48 +0800

    refactor calculateScore and addCards to handle dealer and player actions and add deal button

--- src/components/CardTest.js
+++ src/components/CardTest.js
@@ -5,7 +5,12 @@ import Card from "../classes/Card";
 const CardTest = () => {
   const [deckId, setDeckId] = useState("");
   const [playerCards, setPlayerCards] = useState([]);
-  const [score, setScore] = useState({
+  const [dealerCards, setDealerCards] = useState([]);
+  const [playerScore, setPlayerScore] = useState({
+      softTotal: 0,
+      hardTotal: 0
+  });
+  const [dealerScore, setDealerScore] = useState({
       softTotal: 0,
       hardTotal: 0
   });
@@ -16,19 +21,32 @@ const CardTest = () => {
   }, []);
 
   //INPUT: cards: Array of Objects receieved from API
-  function addToHand(cards) {
+  //       dealer: Boolean - true if adding to dealers hand
+  function addToHand(cards, dealer) {
     let cardsToAdd = [];
     //create a new Card from data receieved and push to array of Cards to add to state
     cards.forEach((card) => {
       cardsToAdd.push(new Card(card.suit, card.value, card.image));
-      calculateScore(card.value);
+      if (dealer) {
+        calculateScore(card.value, dealerScore, setDealerScore);
+      } else {
+        calculateScore(card.value, playerScore, setPlayerScore);
+      }
     });
 
     //add new cards to players cards paying respect to what cards they already have
-    setPlayerCards([...playerCards, ...cardsToAdd]);
+    if (dealer) {
+      setDealerCards([...dealerCards, ...cardsToAdd]);
+    } else {
+      setPlayerCards([...playerCards, ...cardsToAdd]);
+    }
   }
 
-  function calculateScore(value) {
+  /*INPUT: value: card value
+           score: relevant score state
+           setScore: relevant score setter function
+  */
+  function calculateScore(value, score, setScore) {
     // calculates score for aces
     if (value === "ACE") {
       score.hardTotal += 1;
@@ -47,6 +65,12 @@ const CardTest = () => {
     setScore({...score});
   }
 
+  //draw 2 cards for player then for dealer
+  function dealCards() {
+    drawCard(deckId, 2).then((cards) => addToHand(cards, false));
+    drawCard(deckId, 2).then((cards) => addToHand(cards, true));
+  }
+
   return (
     <>
       <h1>card api test</h1>
@@ -58,11 +82,26 @@ const CardTest = () => {
       <button onClick={() => drawCard(deckId, 1).then(addToHand)}>
         Draw 1 Card
       </button>
+      <button onClick={dealCards}>Deal</button>
       {/* Displays score with aces as 11 if score is less than 21, else score with ace as 1 */}
-      <h2>Points: {(score.softTotal > 1) && (score.softTotal > 21 ? score.hardTotal : score.softTotal)}</h2>
+      <h2>Player Points: {(playerScore.softTotal > 1) && (playerScore.softTotal > 21 ? playerScore.hardTotal : playerScore.softTotal)}</h2>
       {/*Display cards value/suit/img*/}
       <h2>Players Cards:</h2>
       {playerCards.map((card, index) => {
+        return (
+          <div key={index}>
+            <p>
+              {card.value} {card.suit}
+            </p>
+            <img alt={`${card.value} ${card.suit}`} src={card.imgString} />
+          </div>
+        );
+      })}
+      {/* Displays score with aces as 11 if score is less than 21, else score with ace as 1 */}
+      <h2>Dealer Points: {(dealerScore.softTotal > 1) && (dealerScore.softTotal > 21 ? dealerScore.hardTotal : dealerScore.softTotal)}</h2>
+      {/*Display cards value/suit/img*/}
+      <h2>Dealers Cards:</h2>
+      {dealerCards.map((card, index) => {
         return (
           <div key={index}>
             <p>
