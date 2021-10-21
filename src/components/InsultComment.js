f347cb79cfc7c47a35fb5d1a64f04b4ae2c86d72
ahnemui <clh.oneil@gmail.com>
2021-10-21 13:02:56 +1100

    insult API instalment with cors-anywhere

--- src/components/InsultComment.js
+++ src/components/InsultComment.js
@@ -1,27 +1,16 @@
 import React, {useEffect, useState} from 'react';
 
-// fetch random insult from API
-// function getRamdomInsult() {
-//     let randomInsult = fetch(`https://evilinsult.com/generate_insult.php?lang=en&type=json`)
-//         .then(response => response.json())
-//         .then(data => data.insult)
-//         .catch(error => console.error(error.message))
-// }
-
-// getRamdomInsult();
 
 const InsultComment = () => {
    const [insult, setInsult] = useState("")
 
     useEffect(()=> {
-        fetch('https://evilinsult.com/generate_insult.php?lang=en&type=json', {
-            mode: 'no-cors',
+        fetch('https://cors-anywhere.herokuapp.com/https://evilinsult.com/generate_insult.php?lang=en&type=json', {
             headers: {
                 'Content-Type': 'application/json'
             }
         })
         .then(response => response.json())
-        .then(data => console.log(data))
         .then(data => setInsult(data.insult))
         .catch(error => console.error(error))
     }, [])
