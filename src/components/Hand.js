2dba3cf3bae148f071689b345e94b08e04458b6c
Nam-fullstack <namnguyen13_@hotmail.com>
2021-10-22 18:29:48 +1100

    Gets cards to be overlayed on top of each other, but additional cards need to keep moving across.

--- src/components/Hand.js
+++ src/components/Hand.js
@@ -15,15 +15,36 @@ const Hand = (props) => {
     boxShadow: 'inset 0 0 0 9px white'
   }
 
+  const faceUpCard = {
+    width:'150px',
+    position: 'absolute',
+    left: '50px'
+  }
+
+  const busted = {
+    color: 'red',
+    fontFamily: 'helvetica',
+    position: 'absolute',
+    left: '200px',
+    top: '400px',
+    // border: '1px solid red',
+  }
+
+  const scoreStyling = {
+    display: 'flex',
+    // border: '1px solid red',
+  }
+
   return (
     <div>
       {/* Displays cards */}
-      <h2>{dealer ? "Dealer: " : "Player: "}
+      <h2 style={scoreStyling}>{dealer ? "Dealer: " : "Player: "}
       {/* Displays score with aces as 11 if score is less than 21, else score with ace as 1
       Dont show points if no cards */}
         {score.highTotal >= 1 &&
           (score.highTotal > 21 ? score.lowTotal : score.highTotal)}
-          {(cards.length > 0 && (bust && <h2>Bust!</h2>))}</h2>
+        <p style={busted}>{(cards.length > 0 && (bust && <h2>BUST!</h2>))}</p>
+      </h2>
       <div style={{display: "flex"}}>
         {cards.map((card, index) => {
           return (
@@ -32,14 +53,16 @@ const Hand = (props) => {
                 <img alt={"face down card"} src={faceDownCard} style={cardBack}  />
               ) : (
                 <>
-                  {/* <p>
-                    {card.value} {card.suit}
-                  </p> */}
+                  {index === 0 ? <img
+                    alt={`${card.value} ${card.suit}`}
+                    src={card.imgString}
+                    width={150} /> 
+                  : 
                   <img
                     alt={`${card.value} ${card.suit}`}
                     src={card.imgString}
-                    width={150}
-                  />
+                    style={faceUpCard} />
+                  }
                 </>
               )}
             </div>
