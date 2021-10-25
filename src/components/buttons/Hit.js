5fb5f3bc6ce4be41c260e11d01be30bc9208f8b6
Nam-fullstack <namnguyen13_@hotmail.com>
2021-10-26 07:01:48 +1100

    Adds hand and hit icons into buttons and adds styling.

--- src/components/buttons/Hit.js
+++ src/components/buttons/Hit.js
@@ -1,10 +1,11 @@
 import Button from "@mui/material/Button";
+import HitIcon from "../../images/hit.png";
 
 const Hit = (props) => {
   const {buttonFunc} = props;
   return (
     <Button variant="contained" size="large" onClick={buttonFunc} style={{marginLeft: '10px'}}>
-      Hit
+      Hit <img src={HitIcon} alt="hit" style={{width: "25px", height: "25px", marginLeft: "4px"}} />  
     </Button>
   );
 };
--- src/components/buttons/Stand.js
+++ src/components/buttons/Stand.js
@@ -1,10 +1,11 @@
 import Button from "@mui/material/Button";
+import Hand from "../../images/hand.png";
 
 const Stand = (props) => {
   const {buttonFunc} = props;
   return (
     <Button variant="contained" size="large" color="error" onClick={buttonFunc} style={{marginLeft: '10px'}}>
-      Stand
+      Stand <img src={Hand} style={{width:'19px', height:'25px', marginLeft:'5px'}} alt='stand icon' />
     </Button>
   );
 };
